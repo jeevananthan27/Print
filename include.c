4c9ccb03ccf10097a57feb28fe1b2c18e3f2aa92
JEEVANANTHAN A <106263059+jeevananthan27@users.noreply.github.com>
2023-03-09 20:56:38 +0530

    Add files via upload

---
+++ include.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+    
+    int a=1;
+    
+    for (printf("a");a<=3;printf("c"))
+    
+    {
+        
+        printf("b");
+        a=a+1;
+        
+    }
+    
+        return 0;
+        
+}
